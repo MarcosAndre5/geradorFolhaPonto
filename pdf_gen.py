5931b16139f9aa983507c0bfaef82e17e487d896
Marcos André <marcosandreazevedo78@gmail.com>
2022-09-18 21:03:50 -0300

    Correção de bug dos domingos das segundas-feiras.

--- pdf_gen.py
+++ pdf_gen.py
@@ -3,62 +3,68 @@ from reportlab.lib.pagesizes import inch, A4
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, PageBreak
 
 class PDFGen:
-    elements = []
+	elementos = []
 
-    def create_new_document(self, linhas, mes, primeiroDiaMes, nomesAquivo):
-        self.doc = SimpleDocTemplate("Folha_de_Pontos.pdf", pagesize=A4, rightMargin=0, leftMargin=0, topMargin=0, bottomMargin=0)
+	def criarNovoDocumento(self, linhas, mes, primeiroDiaMes, nomesAquivo):
+		self.doc = SimpleDocTemplate("Folha_de_Pontos.pdf", pagesize=A4, rightMargin=0, leftMargin=0, topMargin=0, bottomMargin=0)
 
-        # Quanto no sábado, supondo (0 - segunda, 6 - domingo)
-        diferencaDias = (6 - primeiroDiaMes)
-        
-        for name in nomesAquivo:
-            data_table = self.make_table(name, linhas, mes+1, diferencaDias)
+		# Quanto no sábado, supondo (0 - segunda, 6 - domingo)
+		diferencaDias = (6 - primeiroDiaMes)
+		
+		for nome in nomesAquivo:
+			dadosTabela = self.montarTabela(nome, linhas, mes + 1, diferencaDias)
 
-            t = Table(data_table, 11 * [0.65 * inch], (linhas + 1) * [0.34 * inch])
-            t.setStyle(TableStyle(self.get_table_style(linhas, diferencaDias)))
-            
-            self.elements.append(t)
-            self.elements.append(PageBreak())
+			t = Table(dadosTabela, 11 * [0.65 * inch], (linhas + 2) * [0.34 * inch])
+			t.setStyle(TableStyle(self.obterEstiloTabela(linhas + 1, diferencaDias)))
+			
+			self.elementos.append(t)
+			self.elementos.append(PageBreak())
 
-    def make_table(self, name, linhas, mes, diferencaDias):
-        data_table = [[name.upper()]]
+	def montarTabela(self, nome, linhas, mes, diferencaDias):
+		dadosTabela = [[nome.upper()]]
+		
+		sabado = False
+		for i in range(0, linhas + 1):
+			if i == 0:
+				dadosTabela.append(['DATA', 'ENTRADA', 'ASSINATURA', '', '', '', '', '', '', '', 'SAÍDA'])
+			elif i % 7 == diferencaDias:
+				dadosTabela.append(["{:02d}".format(i) + '/' + "{:02d}".format(mes), '', 'SÁBADO', '', '', '', '', '', '', '', ''])
+				sabado = True
+			elif i % 7 == diferencaDias + 1 or sabado == True:
+				dadosTabela.append(["{:02d}".format(i) + '/' + "{:02d}".format(mes), '', 'DOMINGO', '', '', '', '', '', '', '', ''])
+				sabado = False
+			else:
+				dadosTabela.append(["{:02d}".format(i) + '/' + "{:02d}".format(mes), ':', '', '', '', '', '', '', '', '', ':'])
 
-        #data_table.append(['DATA', 'ENTRADA', 'ASSINATURA', '', '', '', '', '', '', '', 'SAÍDA'])
-        
-        for i in range(1, linhas + 1, 1):
-            if (i % 7 == diferencaDias):
-                data_table.append(["{:02d}".format(i) + '/' + "{:02d}".format(mes), ':', 'SÁBADO', '', '', '', '', '', '', '', ':'])
-            elif (i % 7 == diferencaDias+1 or i-1 % 7 == diferencaDias):
-                data_table.append(["{:02d}".format(i) + '/' + "{:02d}".format(mes), ':', 'DOMINGO', '', '', '', '', '', '', '', ':'])
-            else:
-                data_table.append(["{:02d}".format(i) + '/' + "{:02d}".format(mes), ':', '', '', '', '', '', '', '', '', ':'])
+		return dadosTabela
 
-        return data_table
+	def obterEstiloTabela(self, linhas, diferencaDias):
+		estiloTabela = [
+			('ALIGN', (0, 0), (10, linhas), 'CENTER'),
+			('VALIGN', (0, 0), (10, linhas), 'MIDDLE'),
+			('BOX', (0, 0), (10, linhas), 0.25, colors.black),
+			('INNERGRID', (0, 0), (10, linhas), 0.25, colors.black),
+			('SPAN', (0, 0), (10, 0)),
+			('SPAN', (2, 1), (4, 1)),
+			('SPAN', (7, 1), (9, 1))
+		]
 
-    def get_table_style(self, linhas, diferencaDias):
-        table_style = [
-            ('ALIGN', (0, 0), (10, linhas), 'CENTER'),
-            ('VALIGN', (0, 0), (10, linhas), 'MIDDLE'),
-            ('BOX', (0, 0), (10, linhas), 0.25, colors.black),
-            ('INNERGRID', (0, 0), (10, linhas), 0.25, colors.black),
-            ('SPAN', (0, 0), (10, 0)),
-            ('SPAN', (2, 1), (4, 1)),
-            ('SPAN', (7, 1), (9, 1))
-        ]
+		# células grandes para nomes
+		for i in range(1, linhas + 1):
+			estiloTabela.append(('SPAN', (2, i), (9, i)))
+			estiloTabela.append(('SPAN', (7, i), (9, i)))
 
-        # células grandes para nomes
-        for i in range(1, linhas + 1, 1):
-            table_style.append(('SPAN', (2, i), (9, i)))
-            table_style.append(('SPAN', (7, i), (9, i)))
+		# Mudandça de cor para as linhas de sábados de domingos
+		sabado = False
+		for i in range(1, linhas + 1):
+			if i % 7 == diferencaDias:
+				estiloTabela.append(('BACKGROUND', (0, i+1), (11, i+1), colors.gray))
+				sabado = True
+			elif i % 7 == diferencaDias + 1 or sabado == True:
+				estiloTabela.append(('BACKGROUND', (0, i+1), (11, i+1), colors.gray))
+				sabado = False
 
-        # Mudandça de cor para as linhas de sábados de domingos
-        for i in range(1, linhas + 1, 1):
-            if (i % 7 == diferencaDias):
-                table_style.append(('BACKGROUND', (0, i), (11, i), colors.gray))
-            elif(i % 7 == diferencaDias+1 or i-1 % 7 == diferencaDias):
-                table_style.append(('BACKGROUND', (0, i), (11, i), colors.gray))
+		return estiloTabela
 
-        return table_style
-
-    def build(self):
-        self.doc.build(self.elements)
+	def build(self):
+		self.doc.build(self.elementos)
